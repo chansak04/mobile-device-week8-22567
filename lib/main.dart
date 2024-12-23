18ad2cb8390a0f582851c9992a46f134ccfa0491
Deaw <134027539+chansak04@users.noreply.github.com>
2024-12-24 01:34:58 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:week6/view/auth/login.dart';
+import 'package:week6/view/home.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen()
+    );
+  }
+}
